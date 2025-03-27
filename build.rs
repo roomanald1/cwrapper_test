6ebc6d07d0596bb733fd385bffdf69e6ab7effa2
roomanald1 <ronnie.day1@gmail.com>
2025-03-27 17:11:40 +0000

    mostly working example of cwrapper for igraph c

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 extern crate bindgen;
-
-use std::env;
+use std::{env, fs};
 use std::path::PathBuf;
 
 fn main() {
@@ -10,12 +9,21 @@ fn main() {
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .clang_arg("-I/usr/local/include/igraph")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let binding_path = out_path.join("bindings.rs");
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
+        .write_to_file(&binding_path)
         .expect("Couldn't write bindings!");
+
+
+    // Add allow attributes to the generated file
+    let mut contents = fs::read_to_string(&binding_path).unwrap();
+    contents = format!("#[allow(unsafe_code, unused, non_snake_case)]\n{}", contents);
+
+    fs::write(&binding_path, contents).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,56 @@
-use cwrapper_test::version_string;
+use cwrapper_test::{add_edge, add_vertices, clean_graph, empty_graph, find_cycle, is_directed, neighbours, num_edges, num_vertices, version_string};
 
-fn main() {
+fn main() -> Result<(), String> {
     println!("igraph version: {}", version_string());
+    println!("creating graph");
+    let mut graph = empty_graph(true)?;
+    println!("created graph directed={}", is_directed(&mut graph));
+
+    println!("adding nodes 0 - 4");
+    add_vertices(&mut graph, 5)?;
+    println!("added nodes");
+
+    println!("adding edge 0 -> 1");
+    add_edge(&mut graph, 0, 1, Some(0.5))?;
+
+    println!("adding edge 1 -> 0");
+    add_edge(&mut graph, 1, 0, Some(0.3))?;
+
+    println!("adding edge 2 -> 0");
+    add_edge(&mut graph, 2, 0, Some(1.0))?;
+
+    println!("adding edge 0 -> 4");
+    add_edge(&mut graph, 0, 4, Some(5.8))?;
+
+    println!("adding edge 4 -> 2");
+    add_edge(&mut graph, 4, 2, Some(9.0))?;
+
+    let v_count = num_vertices(&graph);
+    let e_count = num_edges(&graph);
+
+    let n_0 = neighbours(&graph, 0)?;
+    println!("n0 {:?}", n_0);
+
+    let n_1 = neighbours(&graph, 1)?;
+    println!("n1 {:?}", n_1);
+
+    let n_2 = neighbours(&graph, 2)?;
+    println!("n2 {:?}", n_2);
+
+    let n_3 = neighbours(&graph, 3)?;
+    println!("n3 {:?}", n_3);
+
+    let n_4 = neighbours(&graph, 4)?;
+    println!("n4 {:?}", n_4);
+
+    let (cycle_v, _) = find_cycle(&graph)?;
+
+    println!("Result: \n \
+    Number of Vertices({v_count}), \n \
+    Number of Edges ({e_count}) \n \
+    Has Cycle v({:?})", cycle_v);
+
+
+    //clean_graph(&mut graph);
+    Ok(())
 }
